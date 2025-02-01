f63df4d189188b1357d92e5de0d79d895fdd6b5f
JakobeG301 <jakobeg301@gmail.com>
2025-02-01 15:42:13 +0100

    Added more logic to the main functionality, bug fixes

--- SimpleCrowdfund.sol
+++ SimpleCrowdfund.sol
@@ -2,132 +2,150 @@
 
 pragma solidity 0.8.28;
 
-contract SimpleCrowdfund{
-
+contract SimpleCrowdfund {
     uint256 public amountRaised = 0;
     bool public campaignEnded;
     bool public goalReached;
     bool public fundsWithdrawned;
 
-    uint256 constant public GOAL = 2e18;  //2 eth in wei
-    uint256 constant public minimalAmount = 1e15;  // ~3$
+    uint256 public constant minimalAmount = 1e15; // ~3$
 
-    address immutable i_owner = payable(msg.sender);
-    uint256 immutable i_timeInitiation = block.timestamp; // Setting up initiation time
-    uint256 immutable public i_secToComplete = 30;//3600 * 24;
-    uint256 immutable public i_deadline = i_timeInitiation + i_secToComplete; // final time to complete the task
+    address public immutable i_owner = payable(msg.sender);
+    uint256 public immutable i_timeInitiation = block.timestamp; // Setting up initiation time
+    uint256 public immutable GOAL = 2e18; //2 eth in wei
+    uint256 public immutable i_secToComplete = 30; //3600 * 24;
+    uint256 public immutable i_deadline = i_timeInitiation + i_secToComplete; // final time to complete the task
 
     address[] public ContributorsList;
 
     event Contributed(address contributor, uint256 amount);
-    event Withdraw(address owner, uint256 amount);    
+    event Withdraw(address owner, uint256 amount);
     event Refunded(address contributor, uint256 amount);
     //event Raised(uint256 amountRaised, uint256 GOAL);
 
-    mapping (address contributor => uint256 amountDonated) public ContributorToAmount;
-    mapping (address contributor => bool isContributor) public AlreadyContributed;
+    mapping(address contributor => uint256 amountDonated) private s_contributorToAmount;
+    mapping(address contributor => bool isContributor) private s_alreadyContributed;
 
-    error NotTheOwner();
-    error ZeroAddress();
-    error ToLittleDonation();
-    error CampaignIsNotEnded();
-    error CampaignIsEnded();
-    error CallFailed();
-    
-    
-    modifier onlyOwner{
-        if(msg.sender != i_owner) {revert NotTheOwner();}
+    error SimpleCrowdfund__NoPermission();
+    error SimpleCrowdfund__ZeroAddress();
+    error SimpleCrowdfund__ToLittleDonation();
+    error SimpleCrowdfund__CampaignIsNotEnded();
+    error SimpleCrowdfund__CampaignIsEnded();
+    error SimpleCrowdfund__CallFailed();
+    error SimpleCrowdfund__TimeOut();
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert SimpleCrowdfund__NoPermission();
         _;
     }
 
-    modifier isWithdrawned{ // test that modifier
-        if(fundsWithdrawned) {revert CallFailed();}
+    modifier isWithdrawned( // test that modifier
+    ) {
+        if (fundsWithdrawned) revert SimpleCrowdfund__CampaignIsEnded();
+        _;
+    }
+    modifier isGoalReached( // test that modifier
+    ) {
+        if (amountRaised >= GOAL) revert SimpleCrowdfund__CampaignIsEnded();
+        if (goalReached) revert SimpleCrowdfund__CampaignIsEnded(); //Second check in case of double withdraw
         _;
     }
 
-    constructor(address _owner, uint256 _secToComplete){
-        if (_owner == address(0)){
-            revert ZeroAddress();
+    constructor(address _owner, uint256 _secToComplete, uint256 _GOAL) {
+        if (_owner == address(0)) {
+            revert SimpleCrowdfund__ZeroAddress();
         }
         i_owner = _owner;
         i_timeInitiation = block.timestamp;
+        GOAL = _GOAL;
         i_deadline = i_timeInitiation + _secToComplete;
     }
 
-    function contribute() payable public{
+    function contribute() public payable isWithdrawned isGoalReached{
         // Check: If a user calls contribute() after the deadline, the call should revert or fail.
-        // The contract should keep track of the total amount raised. Check: Every time function is envoke, check if goal is reached 
-        
-        if(msg.value < minimalAmount){revert ToLittleDonation();}
-        if(amountRaised >= GOAL){revert CampaignIsEnded();}
-        if(timePassed()){revert CampaignIsEnded();}
-        
-        if(AlreadyContributed[msg.sender] == false){
+        // The contract should keep track of the total amount raised. Check: Every time function is envoke, check if goal is reached
+
+        if (msg.value < minimalAmount) revert SimpleCrowdfund__ToLittleDonation();
+        if (amountRaised >= GOAL) goalReached = true;
+        if (timePassed()) revert SimpleCrowdfund__TimeOut();
+
+        if (s_alreadyContributed[msg.sender] == false) {
             ContributorsList.push(msg.sender);
-            ContributorToAmount[msg.sender] = ContributorToAmount[msg.sender] + msg.value;
-            AlreadyContributed[msg.sender] = true;
+            s_contributorToAmount[msg.sender] = s_contributorToAmount[msg.sender] + msg.value;
+            s_alreadyContributed[msg.sender] = true;
             amountRaised = amountRaised + msg.value;
             emit Contributed(msg.sender, msg.value);
-        }
-        
-        else 
-        {
-            ContributorToAmount[msg.sender] = ContributorToAmount[msg.sender] + msg.value;
+            if (amountRaised >= GOAL) goalReached = true;
+
+        } else {
+            s_contributorToAmount[msg.sender] = s_contributorToAmount[msg.sender] + msg.value;
             amountRaised = amountRaised + msg.value;
             emit Contributed(msg.sender, msg.value);
+            if (amountRaised >= GOAL) goalReached = true;
         }
+    
+
     }
 
-    function withdraw() public onlyOwner isWithdrawned{
-        // DONE check: only the Project Owner should be able to withdraw the entire balance. 
-        // DONE check: If the goal is reached on or before the deadline, only the Project Owner should be able to withdraw the entire balance. 
+    receive() external payable {
+            contribute();
+        }
+        
+    fallback() external payable {
+            contribute();
+        }
+
+
+    function withdraw() public onlyOwner isWithdrawned { 
+        // DONE check: only the Project Owner should be able to withdraw the entire balance.
+        // DONE check: If the goal is reached on or before the deadline, only the Project Owner should be able to withdraw the entire balance.
         // DONE check: If the owner tries to call withdraw() before the deadline but the goal isn’t reached yet, it should fail.
         // DONE check: If the user calling withdraw() is not the Project Owner, it should fail.
-        // check: After withdraw user should not be able to contribute more 
-        if(amountRaised >= GOAL){
-            
-            (bool callSuccess, ) = payable(i_owner).call{value: address(this).balance}("");
-            if(!callSuccess){revert CallFailed();}
-            else{emit Withdraw(i_owner, amountRaised);}
-            fundsWithdrawned = true;
-            }
-    }
+        // check: After withdraw user should not be able to contribute more
+        // If the user calling withdraw() is not the Project Owner, it should fail.
 
-    function refund() public{
-        // check: If the goal is not reached by the time the deadline passes, backers should be able to get their ETH back by calling refund()
-        // check:  If the goal is reached or if we are still before the deadline, calling refund() should fail.
-        if(timePassed() && amountRaised < GOAL)
-        {
-            for(uint256 ContributorIndex = 0; ContributorIndex < ContributorsList.length; ContributorIndex++){
-                (bool callSuccess, ) = payable(ContributorsList[ContributorIndex]).call{value: address(this).balance}("");
-                if(!callSuccess){revert CallFailed();}
-                else{emit Withdraw(i_owner, amountRaised);}
-            }
+        if (amountRaised >= GOAL && fundsWithdrawned == false) {
+            (bool callSuccess,) = payable(i_owner).call{value: address(this).balance}("");
+            if (!callSuccess) revert SimpleCrowdfund__CallFailed();
+            else emit Withdraw(i_owner, amountRaised);
+            fundsWithdrawned = true;
+        }
+        else{
+            revert SimpleCrowdfund__CampaignIsNotEnded();
         }
     }
 
-    function timePassed() internal view returns (bool isEnded){  // CHANGE visibility
+
+
+
+
+    function refund() public isWithdrawned{// !!!! Add refund for every backer seperately
+        // check: If the goal is not reached by the time the deadline passes, backers should be able to get their ETH back by calling refund()
+        // check:  If the goal is reached or if we are still before the deadline, calling refund() should fail.
+        if (timePassed() && amountRaised < GOAL) {
+            // for (uint256 ContributorIndex = 0; ContributorIndex < ContributorsList.length; ContributorIndex++) {
+            //     (bool callSuccess,) = payable(ContributorsList[ContributorIndex]).call{value: address(this).balance}("");
+            //     if (!callSuccess) revert SimpleCrowdfund__CallFailed();
+            //     else emit Withdraw(i_owner, amountRaised);
+            if (s_alreadyContributed[msg.sender] == true){
+                (bool callSuccess,) = payable(msg.sender).call{value: s_contributorToAmount[msg.sender]}("");
+                if (!callSuccess) revert SimpleCrowdfund__CallFailed();
+                else emit Refunded(msg.sender, s_contributorToAmount[msg.sender]);
+            }
+            else revert SimpleCrowdfund__NoPermission();
+            // for (uint256 ContributorIndex = 0; ContributorIndex < ContributorsList.length; ContributorIndex++) {
+            //     (bool callSuccess,) = payable(ContributorsList[ContributorIndex]).call{value: address(this).balance}("");
+            //     if (!callSuccess) revert SimpleCrowdfund__CallFailed();
+            //     else emit Withdraw(i_owner, amountRaised);
+            // }
+        }
+    }
+
+    function timePassed() public view returns (bool isEnded) {
         uint256 currentTimestamp = block.timestamp;
-        if (currentTimestamp > i_timeInitiation + i_secToComplete){
+        if (currentTimestamp > i_timeInitiation + i_secToComplete) {
             return true;
         }
     }
-
-    // function isContributor(address _address) public returns (bool included){
-    // }
-
-    receive() external payable{
-        contribute();
-    }
-    fallback() external payable{
-        contribute();
-    }
 }
-// Trash
 
-        // if(amountRaised < GOAL){
-        //     emit Raised(amountRaised, GOAL);}
-        
-        // else {revert CampaignIsEnded();}
-        //     _;
-        // }
